f40d92a417b63e24163116c764c814ccf967f32f
Adam Zerner <adamzerner@protonmail.com>
2024-02-09 23:42:08 -0800

    Read from KV

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,12 @@
-export default () => {
+export default async () => {
+  const kv = await Deno.openKv();
+
+  const user = await kv.get(["users", "alejandra_bby123@yahoo.com"]);
+  const value = (user.value as any).value;
+
   return (
     <div>
-      Hello world
+      Hello world<div>value: {value}</div>
     </div>
   );
 };

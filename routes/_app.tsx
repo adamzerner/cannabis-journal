84101b9c9fded0afc60ad9e2a64c1d6c9c41a4e6
Adam Zerner <adamzerner@protonmail.com>
2024-02-09 22:35:41 -0800

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,16 +5,16 @@ export default ({ Component }: PageProps) => {
   return (
     <html lang="en">
       <head>
+        <title>Cannabis Journal</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Cannabis Journal</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="stylesheet" href={getStylesheetUrl()} />
         <link rel="stylesheet" href="/prism.css" />
-        <script src="/prism.js"></script>
       </head>
       <body>
         <Component />
+        <script src="/prism.js"></script>
       </body>
     </html>
   );

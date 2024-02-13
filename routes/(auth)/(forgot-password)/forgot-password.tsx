22206dd865ac3caf98544ea5b76118fb1e54711f
Adam Zerner <adamzerner@protonmail.com>
2024-02-12 20:51:13 -0800

    Hide reset password stuff

--- routes/(auth)/(forgot-password)/forgot-password.tsx
+++ routes/(auth)/(forgot-password)/forgot-password.tsx
@@ -1,4 +1,4 @@
-import { Button, FormField, H1 } from "rfui";
+import { Button, FormField, H1, Link, Text } from "rfui";
 
 export { handler } from "./(_helpers)/handler.ts";
 
@@ -6,7 +6,17 @@ export default () => {
   return (
     <main>
       <H1>Forgot password</H1>
-      <form method="post" class="flex flex-col gap-5 w-[600px]">
+      <Text>
+        <p>
+          Forgot your password? Contact{" "}
+          <Link href="mailto:adamzerner@protonmail.com">
+            adamzerner@protonmail.com
+          </Link>{" "}
+          for support.
+        </p>
+      </Text>
+      {
+        /* <form method="post" class="flex flex-col gap-5 w-[600px]">
         <FormField
           required
           label="Email"
@@ -15,7 +25,8 @@ export default () => {
           helperText="You'll receive an email with instructions for resetting your password"
         />
         <Button type="submit" class="mt-2 self-start">Submit</Button>
-      </form>
+      </form> */
+      }
     </main>
   );
 };
